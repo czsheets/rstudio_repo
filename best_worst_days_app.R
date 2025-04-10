9f74a1cd929b0e962ab7b32b8195cae7307ef1a7
Charles Sheets <czsheets@gmail.com>
2025-04-10 08:21:54 -0400

    Added dropdowns

--- best_worst_days_app.R
+++ best_worst_days_app.R
@@ -22,23 +22,51 @@ remove_outliers <- function(df, outlier_level = 'months', count_var = 10, remove
     mutate(PCT_change = ifelse(is.na(MATCHED), PCT_change, 0))
 }
 
+# Load SP500 data and preprocess
+sp500 <- new.env()
+getSymbols("^GSPC", env = sp500, src = "yahoo", from = as.Date("1926-01-01"), to = Sys.Date())
+GSPC <- get("GSPC", envir = sp500)
+
+GSPC_df <- GSPC %>% 
+  as.data.frame() %>%
+  tibble()
+
+names(GSPC_df) <- str_remove_all(names(GSPC_df), 'GSPC.')
+GSPC_df$dates <- as.Date(row.names(as.data.frame(GSPC)))
+
+GSPC_df <- GSPC_df %>%
+  select(Close, Adjusted, dates) %>%
+  mutate(PCT_change = (Close - lag(Close)) / lag(Close),
+         days = row_number())
+
+cleaned_df <- GSPC_df %>%
+  mutate(week = 7 * ceiling(as.numeric(days - 5 + 4)/7) + as.Date(5 - 4),
+         month = floor_date(dates, 'month')) %>%
+  group_by(week) %>%
+  mutate(weeks = cur_group_id()) %>%
+  group_by(month) %>%
+  mutate(months = cur_group_id()) %>%
+  ungroup()
+
 # UI for the Shiny app
 ui <- fluidPage(
   theme = bslib::bs_theme(version = 4, bootswatch = "cerulean"),  # Choose a theme
   
-  titlePanel("SP500 Daily Percent Change Analysis"),
+  titlePanel("Clustering Best and Worst Periods of SP500 Returns"),
   
   sidebarLayout(
     sidebarPanel(
-      sliderInput("count_var", "Outlier Removal Count:", min = 10, max = 200, value = 100, step = 10),
-      selectInput("outlier_level", "Outlier Level:", choices = c("weeks", "months"), selected = "months"),
-      actionButton("update", "Update Graphs")
+      sliderInput("count_var", "Outlier Removal Count:", min = 0, max = 100, value = 0, step = 1),
+      selectInput("outlier_level", "Time period removed:", choices = c("days", "weeks", "months"), selected = "days"),
+      selectInput('remove_type', 'Return result type to remove', choices = c('high', 'low'), selected = 'high'),
+      selectInput('graph_type', 'Select graph type', choices = c('standard', 'log scale'), selected = 'standard'),
+      actionButton("update", "Create/update Graphs")
     ),
     
     mainPanel(
       plotOutput("percentChangePlot"),
-      plotOutput("sp500ValuePlot"),
-      plotOutput("logValuePlot"),
+      plotOutput("longitudinal_returns"),
+      # plotOutput("logValuePlot"),
       verbatimTextOutput("annualReturnText")
     )
   )
@@ -47,38 +75,12 @@ ui <- fluidPage(
 # Server logic for the Shiny app
 server <- function(input, output) {
   
-  # Load SP500 data and preprocess
-  sp500 <- new.env()
-  getSymbols("^GSPC", env = sp500, src = "yahoo", from = as.Date("1926-01-01"), to = Sys.Date())
-  GSPC <- get("GSPC", envir = sp500)
-  
-  GSPC_df <- GSPC %>% 
-    as.data.frame() %>%
-    tibble()
-  
-  names(GSPC_df) <- str_remove_all(names(GSPC_df), 'GSPC.')
-  GSPC_df$dates <- as.Date(row.names(as.data.frame(GSPC)))
-  
-  GSPC_df <- GSPC_df %>%
-    select(Close, Adjusted, dates) %>%
-    mutate(PCT_change = (Close - lag(Close)) / lag(Close),
-           days = row_number())
-  
-  cleaned_df <- GSPC_df %>%
-    mutate(week = 7 * ceiling(as.numeric(days - 5 + 4)/7) + as.Date(5 - 4),
-           month = floor_date(dates, 'month')) %>%
-    group_by(week) %>%
-    mutate(weeks = cur_group_id()) %>%
-    group_by(month) %>%
-    mutate(months = cur_group_id()) %>%
-    ungroup()
-  
   # Reactive for filtered data
   filtered_df <- reactive({
     remove_outliers(cleaned_df, 
                     outlier_level = input$outlier_level,
                     count_var = input$count_var, 
-                    remove = 'high')
+                    remove = input$remove_type)
   })
   
   # Calculate returns and annual returns
@@ -88,9 +90,9 @@ server <- function(input, output) {
     initial_value <- filtered_data$Close[1]
     new_values <- initial_value * cumprod(1 + coalesce(filtered_data$PCT_change, 0))
     final_value <- new_values[length(new_values)]
-    
     total_return <- (final_value / initial_value) - 1
     num_years <- as.numeric(difftime(max(filtered_data$days), min(filtered_data$days), units = "days")) / 365.25
+    print(num_years)
     orig_annual_return <- (1 + (orig_max_value/initial_value)-1)^(1 / num_years) - 1
     annual_return <- (1 + total_return)^(1 / num_years) - 1
     
@@ -114,37 +116,37 @@ server <- function(input, output) {
         labs(y = 'Percent change', x = 'Date', title = 'Daily Change')
     })
     
-    # Plot 2: SP500 Value
-    output$sp500ValuePlot <- renderPlot({
-      filtered_data %>%
-        mutate(tab = 'Filtered', Close = new_values) %>%
-        select(-MATCHED) %>%
-        rbind(cleaned_df %>%
-                mutate(tab = 'Original')) %>%
-        ggplot(aes(days, Close)) + 
-        geom_line() + 
-        facet_wrap(~tab) + 
-        theme_minimal() +
-        labs(y = 'SP500 value', x = 'Date', 
-             title = paste('Total values removing top', input$count_var, input$outlier_level),
-             subtitle = paste0('Loss of ', 
-                               round((orig_annual_return/annual_return - 1) * 100, 1),
-                               '%'))
-    })
-    
-    # Plot 3: Log-transformed SP500 Value
-    output$logValuePlot <- renderPlot({
-      filtered_data %>%
-        mutate(tab = 'Filtered', Close = log(new_values)) %>%
-        select(-MATCHED) %>%
-        rbind(cleaned_df %>%
-                mutate(tab = 'Original', Close = log(Close))) %>%
-        ggplot(aes(days, Close)) + 
-        geom_line() + 
-        facet_wrap(~tab, nrow = 2) + 
-        theme_minimal() +
-        labs(y = 'SP500 log value', x = 'Date',
-             title = paste('Total values removing top', input$count_var, input$outlier_level))
+    # Plot 2: longitudinal returns
+    output$longitudinal_returns <- renderPlot({
+      if(input$graph_type == 'standard'){
+      
+        filtered_data %>%
+          mutate(tab = 'Filtered', Close = new_values) %>%
+          select(-MATCHED) %>%
+          rbind(cleaned_df %>%
+                  mutate(tab = 'Original')) %>%
+          ggplot(aes(days, Close)) + 
+          geom_line() + 
+          facet_wrap(~tab) + 
+          theme_minimal() +
+          labs(y = 'SP500 value', x = 'Date', 
+               title = paste('Total values removing top', input$count_var, input$outlier_level),
+               subtitle = paste0('Loss of ', 
+                                 round((orig_annual_return/annual_return - 1) * 100, 1),
+                                 '%'))
+      } else {
+        filtered_data %>%
+          mutate(tab = 'Filtered', Close = log(new_values)) %>%
+          select(-MATCHED) %>%
+          rbind(cleaned_df %>%
+                  mutate(tab = 'Original', Close = log(Close))) %>%
+          ggplot(aes(days, Close)) + 
+          geom_line() + 
+          facet_wrap(~tab, nrow = 2) + 
+          theme_minimal() +
+          labs(y = 'SP500 log value', x = 'Date',
+               title = paste('Total values removing top', input$count_var, input$outlier_level))
+      }
     })
   })
 }
